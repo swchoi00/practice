aedc23c0a888ae0ce10ede4ab788ae85bd0fbf71
sw <celpic1@gmail.com>
2023-08-10 14:12:53 +0900

    헤더작업

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <div>헤더작업</div>
       <Routes>
         <Route path='/' element={<div>메인페이지</div>} />
         <Route path='/new' element={<div>새글페이지</div>} />
